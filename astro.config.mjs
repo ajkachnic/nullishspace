fdd35627a2ab64a7611c9b482cf221878eda4882
Andrew Kachnic <ajkachnic@protonmail.com>
2023-06-03 09:09:34 -0400

    Add SSR support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import UnoCSS from 'unocss/astro';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import UnoCSS from "unocss/astro";
 import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), UnoCSS(), preact()]
+  output: "hybrid",
+  experimental: {
+    hybridOutput: true
+  },
+  site: "https://nullish.space",
+  integrations: [mdx(), sitemap(), UnoCSS(), preact()],
+  adapter: vercel()
 });
